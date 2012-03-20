7469ed7a30b92e1b421ea86e196a22534f9e2a41
Michael Opdenacker <michael.opdenacker@free-electrons.com>
2012-03-20 22:03:42 +0100

    Added new example

---
+++ code/hello-param/hello_param.c
@@ -0,0 +1,30 @@
+/* hello_param.c */
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/moduleparam.h>
+
+MODULE_LICENSE("GPL");
+
+/* A couple of parameters that can be passed in: how many times we say
+   hello, and to whom */
+
+static char *whom = "world";
+module_param(whom, charp, 0);
+static int howmany = 1;
+module_param(howmany, int, 0);
+
+static int __init hello_init(void)
+{
+    int i;
+    for (i = 0; i < howmany; i++)
+    	pr_debug(KERN_ALERT "(%d) Hello, %s\n", i, whom);
+    return 0;
+}
+
+static void __exit hello_exit(void)
+{
+    pr_debug(KERN_ALERT "Goodbye, cruel %s\n", whom);
+}
+
+module_init(hello_init);
+module_exit(hello_exit);
