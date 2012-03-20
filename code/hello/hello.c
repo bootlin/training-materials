1e78c00f3ae580b01376297d02897ca4c2dbfc49
Michael Opdenacker <michael.opdenacker@free-electrons.com>
2012-03-20 21:46:46 +0100

    Code examples mentioned in the slides
    Tested with 3.0

---
+++ code/hello/hello.c
@@ -0,0 +1,23 @@
+/* hello.c */
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
+
+static int __init hello_init(void)
+{
+    pr_alert("Good morrow");
+    pr_alert("to this fair assembly.\n");
+    return 0;
+}
+
+static void __exit hello_exit(void)
+{
+    pr_alert("Alas, poor world, what treasure");
+    pr_alert("hast thou lost!\n");
+}
+
+module_init(hello_init);
+module_exit(hello_exit);
+MODULE_LICENSE("GPL");
+MODULE_DESCRIPTION("Greeting module");
+MODULE_AUTHOR("William Shakespeare");
