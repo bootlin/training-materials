a4775a5f56be2c3f76eaab9a95421147a87adbb2
Michael Opdenacker <michael.opdenacker@free-electrons.com>
2014-02-04 13:29:21 +0100

    Kernel slides: simplify hello.c example
    - There was a missing space at the beginning of the second
      pr_alert string (the first one has no \n)
    
    - Put the code download URL in cgit, easier to keep in sync
    
    - Misc minor fixes
    
    Signed-off-by: Michael Opdenacker <michael.opdenacker@free-electrons.com>

--- code/hello/hello.c
+++ code/hello/hello.c
@@ -5,15 +5,13 @@
 
 static int __init hello_init(void)
 {
-    pr_alert("Good morrow");
-    pr_alert("to this fair assembly.\n");
+    pr_alert("Good morrow to this fair assembly.\n");
     return 0;
 }
 
 static void __exit hello_exit(void)
 {
-    pr_alert("Alas, poor world, what treasure");
-    pr_alert("hast thou lost!\n");
+    pr_alert("Alas, poor world, what treasure hast thou lost!\n");
 }
 
 module_init(hello_init);
