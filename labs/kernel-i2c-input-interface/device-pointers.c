7d097134c5b06c2c3799e9d2832152fd2bb6d24d
Maxime Ripard <maxime.ripard@free-electrons.com>
2014-02-17 10:27:04 +0100

    This was already requested in a previous section of the instructions
    Signed-off-by: Maxime Ripard <maxime.ripard@free-electrons.com>

--- labs/kernel-i2c-input-interface/device-pointers.c
+++ labs/kernel-i2c-input-interface/device-pointers.c
@@ -2,5 +2,4 @@
         nunchuk->polled_input = polled_input;
         polled_input->private = nunchuk;
         i2c_set_clientdata(client, nunchuk);
-        input = polled_input->input;
         input->dev.parent = &client->dev;
