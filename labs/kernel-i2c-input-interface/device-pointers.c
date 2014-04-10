cbe68cb46d2473d3fee6f5b00f73b1ffff4b03d0
Michael Opdenacker <michael.opdenacker@free-electrons.com>
2014-04-10 17:15:39 +0200

    Kernel nunchuk/input lab: add missing C line
    - Add the initialization of the 'input' variable.
      Otherwise, the instructions are missing something,
      and are refering to 'input' when it hasn't been initialized yet.
    
    Signed-off-by: Michael Opdenacker <michael.opdenacker@free-electrons.com>

--- labs/kernel-i2c-input-interface/device-pointers.c
+++ labs/kernel-i2c-input-interface/device-pointers.c
@@ -2,4 +2,5 @@
         nunchuk->polled_input = polled_input;
         polled_input->private = nunchuk;
         i2c_set_clientdata(client, nunchuk);
+	input = polled_input->input;
         input->dev.parent = &client->dev;
