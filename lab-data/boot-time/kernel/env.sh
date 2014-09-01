d89ce2ae0b84f393c767060cf8115d2f82dcb2cf
Michael Opdenacker <michael.opdenacker@free-electrons.com>
2014-09-01 22:37:51 +0200

    Fix boot time lab directory name too
    Signed-off-by: Michael Opdenacker <michael.opdenacker@free-electrons.com>

--- lab-data/boot-time/kernel/env.sh
+++ lab-data/boot-time/kernel/env.sh
@@ -1,3 +1,3 @@
 export ARCH=arm
 export CROSS_COMPILE=arm-buildroot-linux-uclibcgnueabi-
-export PATH=$PATH:/opt/felabs/boottime/buildroot/buildroot-at91/output/host/usr/bin/
+export PATH=$PATH:/opt/boot-time-labs/buildroot/buildroot-at91/output/host/usr/bin/
