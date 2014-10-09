ac44ac8a44532d0496342b5c001147e5255df79c
Michael Opdenacker <michael.opdenacker@free-electrons.com>
2014-10-09 16:54:21 +0200

    Kernel input lab: simplify instructions
    - Stop using our device structure while the polling function
      already has the polled_input structure
    
    Signed-off-by: Michael Opdenacker <michael.opdenacker@free-electrons.com>

--- labs/kernel-i2c-input-interface/input-notification.c
+++ labs/kernel-i2c-input-interface/input-notification.c
@@ -1,6 +1,6 @@
-        input_event(nunchuk->polled_input->input,
+        input_event(polled_input->input,
                     EV_KEY, BTN_Z, zpressed);
-        input_event(nunchuk->polled_input->input,
+        input_event(polled_input->input,
                     EV_KEY, BTN_C, cpressed);
 
-        input_sync(nunchuk->polled_input->input);
+        input_sync(polled_input->input);
