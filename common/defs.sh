81c57c57208608c23479ed5aae2e2fadb6bee915
Michael Opdenacker <michael.opdenacker@free-electrons.com>
2016-11-25 15:03:55 +0100

    Add automatic generation of "sysdev-4d" materials
    Signed-off-by: Michael Opdenacker <michael.opdenacker@free-electrons.com>

--- common/defs.sh
+++ common/defs.sh
@@ -1,2 +1,2 @@
 # Definitions for automated scripts processing the materials
-ALL_COURSES="embedded-linux linux-kernel android yocto buildroot boot-time"
+ALL_COURSES="embedded-linux embedded-linux-4days linux-kernel android yocto buildroot boot-time"
