3dcfa07f5e3bb6be4d3dc501eebd7ef8c04d5382
Paul Kocialkowski <paul.kocialkowski@bootlin.com>
2019-08-26 21:42:01 +0200

    graphics: Split off major parts
    Signed-off-by: Paul Kocialkowski <paul.kocialkowski@bootlin.com>

--- mk/graphics.mk
+++ mk/graphics.mk
@@ -2,7 +2,7 @@ GRAPHICS_SLIDES = \
 		first-slides \
 		about-us \
 		course-information-title \
-		beagleboneblack-board \
 		course-information \
-		graphics-introduction \
-		last-slides
+		graphics-theory \
+		graphics-hardware \
+		graphics-software
