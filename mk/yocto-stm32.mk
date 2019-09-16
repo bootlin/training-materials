6f5b25d3d9312177abde7862e00407dc1f4d67bf
Alexandre Belloni <alexandre.belloni@bootlin.com>
2019-09-16 11:10:13 +0200

    yocto: port on STM32MP157

---
+++ mk/yocto-stm32.mk
@@ -0,0 +1,42 @@
+YOCTO_STM32_SLIDES    = \
+		first-slides \
+		about-us \
+		course-information-title \
+		discovery-board \
+		yocto-course-outline \
+		course-information \
+		setup-lab \
+		buildroot-yocto-introduction \
+		yocto-overview \
+		yocto-basics \
+		yocto-build-lab \
+		yocto-advanced \
+		yocto-advanced-lab \
+		yocto-recipe-basics \
+		yocto-recipe-basics-lab \
+		yocto-recipe-advanced \
+		yocto-layer \
+		yocto-layer-lab \
+		yocto-bsp \
+		yocto-bsp-lab \
+		yocto-layer-distro \
+		yocto-image \
+		yocto-image-lab \
+		yocto-licensing \
+		yocto-recipe-extra \
+		yocto-sdk \
+		yocto-sdk-lab \
+		yocto-runtime-package-management \
+		yocto-resources \
+		last-slides
+
+YOCTO_STM32_LABS    = setup \
+		yocto-first-build-stm32 \
+		yocto-advanced-configuration-stm32 \
+		yocto-add-application \
+		yocto-layer \
+		yocto-extend-recipe \
+		yocto-custom-machine \
+		yocto-custom-image \
+		yocto-sdk \
+		yocto-sdk-eclipse
