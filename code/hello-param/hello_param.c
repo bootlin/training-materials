73d041f4265a8a8c3bd440a24d425a08b490c58b
Michael Opdenacker <michael.opdenacker@free-electrons.com>
2019-11-26 10:39:53 +0100

    Module example improvements
    - Add MODULE_PARM_DESC (useful to know)
    - Fix checkpatch issues in example code
    
    Signed-off-by: Michael Opdenacker <michael.opdenacker@free-electrons.com>

--- code/hello-param/hello_param.c
+++ code/hello-param/hello_param.c
@@ -1,28 +1,30 @@
+// SPDX-License-Identifier: GPL-2.0
 /* hello_param.c */
 #include <linux/init.h>
 #include <linux/module.h>
 
 MODULE_LICENSE("GPL");
 
-/* A couple of parameters that can be passed in: how many times we say
-   hello, and to whom */
-
 static char *whom = "world";
 module_param(whom, charp, 0);
+MODULE_PARM_DESC(whom, "Recipient of the hello message");
+
 static int howmany = 1;
 module_param(howmany, int, 0);
+MODULE_PARM_DESC(howmany, "Number of greetings");
 
 static int __init hello_init(void)
 {
-    int i;
-    for (i = 0; i < howmany; i++)
-	pr_alert("(%d) Hello, %s\n", i, whom);
-    return 0;
+	int i;
+
+	for (i = 0; i < howmany; i++)
+		pr_alert("(%d) Hello, %s\n", i, whom);
+	return 0;
 }
 
 static void __exit hello_exit(void)
 {
-    pr_alert("Goodbye, cruel %s\n", whom);
+	pr_alert("Goodbye, cruel %s\n", whom);
 }
 
 module_init(hello_init);
--- code/hello/hello.c
+++ code/hello/hello.c
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: GPL-2.0
 /* hello.c */
 #include <linux/init.h>
 #include <linux/module.h>
@@ -5,13 +6,13 @@
 
 static int __init hello_init(void)
 {
-    pr_alert("Good morrow to this fair assembly.\n");
-    return 0;
+	pr_alert("Good morrow to this fair assembly.\n");
+	return 0;
 }
 
 static void __exit hello_exit(void)
 {
-    pr_alert("Alas, poor world, what treasure hast thou lost!\n");
+	pr_alert("Alas, poor world, what treasure hast thou lost!\n");
 }
 
 module_init(hello_init);
