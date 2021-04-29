9557dae1d27f1e62584aec55e85b2b2b7b64c1a0
Michael Opdenacker <michael.opdenacker@bootlin.com>
2021-04-29 12:55:20 +0200

    Kernel input lab: use input_report_key() instead of input_event()
    As recommended in the slides
    
    Signed-off-by: Michael Opdenacker <michael.opdenacker@bootlin.com>
    Reported-by: Miquèl Raynal <miquel.raynal@bootlin.com>

--- labs/kernel-i2c-input-interface/input-notification.c
+++ labs/kernel-i2c-input-interface/input-notification.c
@@ -1,3 +1,3 @@
-input_event(input, EV_KEY, BTN_Z, zpressed);
-input_event(input, EV_KEY, BTN_C, cpressed);
+input_report_key(input, BTN_Z, zpressed);
+input_report_key(input, BTN_C, cpressed);
 input_sync(input);
