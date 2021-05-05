482bbeb95016f7ee618e39a5054ab4eecb766ab8
Miquel Raynal <miquel.raynal@bootlin.com>
2021-05-05 23:05:39 +0200

    linux: serial-lab: Rename the main structure name
    It is probably better to call this structure serial than dev to avoid confusion
    with the device structure of the device model.
    
    Signed-off-by: Miquel Raynal <miquel.raynal@bootlin.com>

--- labs/kernel-serial-iomem/uart-line-reset.c
+++ labs/kernel-serial-iomem/uart-line-reset.c
@@ -1,3 +1,3 @@
 /* Soft reset */
-reg_write(dev, UART_FCR_CLEAR_RCVR | UART_FCR_CLEAR_XMIT, UART_FCR);
-reg_write(dev, 0x00, UART_OMAP_MDR1);
+reg_write(serial, UART_FCR_CLEAR_RCVR | UART_FCR_CLEAR_XMIT, UART_FCR);
+reg_write(serial, 0x00, UART_OMAP_MDR1);
