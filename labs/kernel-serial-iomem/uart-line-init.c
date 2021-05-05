7faf8cccb692c5d8b6e1e6a51dc43ae44579e2ae
Miquel Raynal <miquel.raynal@bootlin.com>
2021-05-05 23:05:39 +0200

    linux: serial-lab: Rename the main structure name
    It is probably better to call this structure serial than dev to avoid confusion
    with the device structure of the device model.
    
    Signed-off-by: Miquel Raynal <miquel.raynal@bootlin.com>

--- labs/kernel-serial-iomem/uart-line-init.c
+++ labs/kernel-serial-iomem/uart-line-init.c
@@ -3,9 +3,9 @@
 of_property_read_u32(pdev->dev.of_node, "clock-frequency",
                      &uartclk);
 baud_divisor = uartclk / 16 / 115200;
-reg_write(dev, 0x07, UART_OMAP_MDR1);
-reg_write(dev, 0x00, UART_LCR);
-reg_write(dev, UART_LCR_DLAB, UART_LCR);
-reg_write(dev, baud_divisor & 0xff, UART_DLL);
-reg_write(dev, (baud_divisor >> 8) & 0xff, UART_DLM);
-reg_write(dev, UART_LCR_WLEN8, UART_LCR);
+reg_write(serial, 0x07, UART_OMAP_MDR1);
+reg_write(serial, 0x00, UART_LCR);
+reg_write(serial, UART_LCR_DLAB, UART_LCR);
+reg_write(serial, baud_divisor & 0xff, UART_DLL);
+reg_write(serial, (baud_divisor >> 8) & 0xff, UART_DLM);
+reg_write(serial, UART_LCR_WLEN8, UART_LCR);
