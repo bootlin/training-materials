98ca795fc5becd6fe1c681bfa14904f8afd53ca2
Miquel Raynal <miquel.raynal@bootlin.com>
2021-05-05 23:07:51 +0200

    linux: serial: Check return value from of_property_read_u32()
    The return value should be checked otherwise we might end up working with a
    wrong uartclk value.
    
    Signed-off-by: Miquel Raynal <miquel.raynal@bootlin.com>

--- labs/kernel-serial-iomem/uart-line-init.c
+++ labs/kernel-serial-iomem/uart-line-init.c
@@ -1,7 +1,13 @@
 /* Configure the baud rate to 115200 */
 
-of_property_read_u32(pdev->dev.of_node, "clock-frequency",
-                     &uartclk);
+ret = of_property_read_u32(pdev->dev.of_node, "clock-frequency",
+			   &uartclk);
+if (ret) {
+	dev_err(&pdev->dev,
+		"clock-frequency property not found in Device Tree\n");
+	return ret;
+}
+
 baud_divisor = uartclk / 16 / 115200;
 reg_write(serial, 0x07, UART_OMAP_MDR1);
 reg_write(serial, 0x00, UART_LCR);
