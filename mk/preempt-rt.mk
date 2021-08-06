3ff5951903b439fffad2cd1b46a20116e28f8053
Maxime Chevallier <maxime.chevallier@bootlin.com>
2021-08-06 16:33:53 +0200

    slides, labs: add slides and labs for new PREEMPT_RT training course
    Signed-off-by: Maxime Chevallier <maxime.chevallier@bootlin.com>

---
+++ mk/preempt-rt.mk
@@ -0,0 +1,20 @@
+PREEMPT_RT_SLIDES    = \
+	first-slides \
+	about-us \
+	course-information-title \
+	realtime-linux-realtime-systems \
+	realtime-linux-preempt-rt \
+	realtime-linux-setup-lab \
+	realtime-linux-hardware \
+	realtime-linux-benchmarking \
+	realtime-linux-configuration \
+	realtime-linux-benchmark-lab \
+	realtime-linux-application-development \
+	realtime-linux-application-development-lab \
+	last-slides
+
+PREEMPT_RT_LABS    = setup \
+		preempt-rt-patch-download \
+		preempt-rt-benchmarking \
+		preempt-rt-application-development \
+		last-slides
