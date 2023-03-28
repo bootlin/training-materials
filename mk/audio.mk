69196764ad1f91f706391a27cd1377565101aa96
Alexandre Belloni <alexandre.belloni@bootlin.com>
2023-03-28 18:07:40 +0200

    audio: Introduce audio training
    Signed-off-by: Alexandre Belloni <alexandre.belloni@bootlin.com>

---
+++ mk/audio.mk
@@ -0,0 +1,19 @@
+AUDIO_SLIDES = \
+	first-slides \
+	about-us \
+	course-information \
+	audio-sound \
+	audio-hardware \
+	audio-asoc \
+	audio-asoc-codec \
+	audio-asoc-component-controls \
+	audio-regmap \
+	audio-asoc-component-callbacks \
+	audio-auxiliary \
+	audio-asoc-DAPM \
+	audio-asoc-cpu \
+	audio-alsa-lib \
+	audio-alsa-utils \
+	audio-debugging \
+	audio-gstreamer \
+	last-slides
