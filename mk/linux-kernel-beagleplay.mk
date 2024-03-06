c8e15ab64ed710e1ab2fcad7be007cecbb870651
Romain Gantois <romain.gantois@bootlin.com>
2024-03-06 11:55:52 +0100

    kernel-beagleplay: Add I2C labs
    The pinmuxing lab had to be changed because the relevant I2C bus is already
    muxed correctly in the Beagle Play device tree. We ask the trainee to delete
    the pinmuxing configuration then add it back to observe the results.
    
    Signed-off-by: Romain Gantois <romain.gantois@bootlin.com>

--- mk/linux-kernel-beagleplay.mk
+++ mk/linux-kernel-beagleplay.mk
@@ -4,3 +4,7 @@ LINUX_KERNEL_BEAGLEPLAY_LABS   = setup \
 				kernel-beagleplay-board-setup \
 				kernel-beagleplay-compiling-and-nfs-booting \
 				kernel-module-simple \
+				kernel-beagleplay-i2c-describing-hardware \
+				kernel-beagleplay-i2c-multiplexing \
+				kernel-i2c-communication \
+				kernel-i2c-input-interface \
