b471eb13c8d1362eabb2287b6c739d42e9ac6bb8
Romain Gantois <romain.gantois@bootlin.com>
2024-03-06 10:16:41 +0100

    kernel-beagleplay: Add simple module lab
    Signed-off-by: Romain Gantois <romain.gantois@bootlin.com>

--- mk/linux-kernel-beagleplay.mk
+++ mk/linux-kernel-beagleplay.mk
@@ -3,3 +3,4 @@ LINUX_KERNEL_BEAGLEPLAY_LABS   = setup \
 				kernel-sources-exploring \
 				kernel-beagleplay-board-setup \
 				kernel-beagleplay-compiling-and-nfs-booting \
+				kernel-module-simple \
