b01d1ff716cfc6c1a456015b697e22a85fceebbd
Romain Gantois <romain.gantois@bootlin.com>
2024-03-06 10:06:23 +0100

    kernel-beagleplay: Add kernel compilation and NFS lab
    The main changes for this lab are in the kernel configuration instructions.
    
    Signed-off-by: Romain Gantois <romain.gantois@bootlin.com>

--- mk/linux-kernel-beagleplay.mk
+++ mk/linux-kernel-beagleplay.mk
@@ -1,3 +1,5 @@
 LINUX_KERNEL_BEAGLEPLAY_LABS   = setup \
 				kernel-sources-download \
 				kernel-sources-exploring \
+				kernel-beagleplay-board-setup \
+				kernel-beagleplay-compiling-and-nfs-booting \
