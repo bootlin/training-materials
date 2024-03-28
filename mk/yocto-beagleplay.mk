3ef6c99bf9650170e44e53ae3bc24e83eb3ada89
Thomas Petazzoni <thomas.petazzoni@bootlin.com>
2024-03-28 10:39:44 +0100

    Add minimal structure for Yocto course on BeaglePlay
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

---
+++ mk/yocto-beagleplay.mk
@@ -0,0 +1,47 @@
+YOCTO_BEAGLEPLAY_SLIDES    = \
+		first-slides \
+		about-us \
+		course-information-title \
+		yocto-shopping-list \
+		beagleboneblack-board \
+		discovery-board-dk1 \
+		course-information \
+		setup-lab \
+		buildroot-yocto-introduction \
+		yocto-overview \
+		yocto-basics \
+		yocto-build-lab \
+		yocto-advanced \
+		yocto-advanced-lab \
+		yocto-recipe-basics \
+		yocto-recipe-basics-lab \
+		yocto-recipe-advanced \
+		yocto-layer \
+		yocto-layer-lab \
+		yocto-bsp \
+		yocto-bsp-lab \
+		yocto-layer-distro \
+		yocto-image \
+		yocto-image-lab \
+		yocto-recipe-extra \
+		yocto-licensing \
+		yocto-sdk \
+		yocto-sdk-lab \
+		yocto-devtool \
+		yocto-devtool-lab \
+		yocto-layer-management \
+		yocto-runtime-package-management \
+		yocto-resources \
+		last-slides \
+		yocto-extra-slides
+
+YOCTO_BEAGLEPLAY_LABS    = setup \
+		yocto-first-build \
+		yocto-advanced-configuration \
+		yocto-add-application \
+		yocto-layer \
+		yocto-extend-recipe \
+		yocto-custom-machine \
+		yocto-custom-image \
+		yocto-sdk \
+		yocto-devtool
