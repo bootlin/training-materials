bootlin/training-materials

commit 178c0d27eb146e6410bff8dea02ba2e3ca065db1

debugging: labs: use proper kprobe macro when targeting a syscall
The current way of hooking the lab tracing program to the execve system
call involves the BPF_KPROBE macro from libbpf. It woks fine for arm
because this architecture is not using system call wrappers in the
kernel. If we try to apply the same kind of program on an architecture
having CONFIG_ARCH_HAS_SYSCALL_WRAPPER, the bpf program will read
garbage, because the arguments are passed in a slightly different way:

bpf_trace_printk: New process 959 running program <0x88>m
bpf_trace_printk: New process 960 running program m
bpf_trace_printk: New process 961 running program <0x88>m
bpf_trace_printk: New process 962 running program m
bpf_trace_printk: New process 963 running program <0x88>m
bpf_trace_printk: New process 964 running program m
bpf_trace_printk: New process 965 running program <0x88>m
bpf_trace_printk: New process 966 running program m
[...]

libbpf offers some more specific macros to automatically deal with this
issue. Replace BPF_KPROBE with BPF_KPROBE_SYSCALL to leverage libbpf
capability to handle properly syscalls arguments in kprobes.

Signed-off-by: Alexis Lothoré <alexis.lothore@bootlin.com>

lab-data/debugging/nfsroot/root/ebpf/libbpf/trace_programs.bpf.c
@@ -5,8 +5,8 @@
 
 #define MAX_FILENAME_LEN 32
 
-SEC("kprobe/sys_execve")
-int BPF_KPROBE(trace_execve, const char *path, char *const _Nullable argv[],
+SEC("ksyscall/execve")
+int BPF_KPROBE_SYSCALL(trace_execve, const char *path, char *const _Nullable argv[],
                char *const _Nullable envp[])
 
 {

labs/debugging-ebpf-libbpf/debugging-ebpf-libbpf.tex
@@ -22,7 +22,7 @@ The first step is to prepare our bpf program:
       \item This program may access some kernel structures at some point, so it has been prepared to benefit from CO-RE (to remain compatible between different kernel versions), that's why it depends on a \path{vmlinux.h} header that we will have to generate.
       \item In order to manipulate kprobes, the program needs some libbpf header, and because the data manipulated by kprobes changes with the platform (it directly uses registers), we need to define the target architecture with \code{__TARGET_ARCH_arm}
       \item The code uses the \code{SEC} macro to place the eBPF program in a specific section: libbpf will use this section to learn about the program type and attach point
-      \item It also uses the \code{BPF_KPROBE} macro to allow to get access to already-interpreted arguments from the kprobes: without this macro, we would have to identify the relevant registers to parse the targeted function arguments
+      \item It also uses the \code{BPF_KPROBE_SYSCALL} macro to allow to get access to already-interpreted arguments from the kprobes: without this macro, we would have to identify the relevant registers to parse the targeted function arguments
       \item Be careful that the \code{bpf_trace_printk} is not the same helper as the one used with BCC, and so the way to call it is slightly different
     \end{itemize}
   \item You will first need to generate the vmlinux header used in the eBPF program. You can use bpftool to do so:

commit c56f13b3c2e2e8a1c575d06c3b6e377e0b157aa3

debugging: labs: add missing header in ebpf base program
BPF_KPROBE_SYSCALL needs the BPF_CORE_READ macro, but the bpf_tracing
header does not automatically includes bpf_core_read.h

Include the missing header to fix the related build error

Fixes: 178c0d27eb14 ("debugging: labs: use proper kprobe macro when targeting a syscall")
Signed-off-by: Alexis Lothoré <alexis.lothore@bootlin.com>

lab-data/debugging/nfsroot/root/ebpf/libbpf/trace_programs.bpf.c
@@ -2,6 +2,7 @@
 #include <bpf/bpf_helpers.h>
 #define __TARGET_ARCH_arm
 #include <bpf/bpf_tracing.h>
+#include <bpf/bpf_core_read.h>
 
 #define MAX_FILENAME_LEN 32
 