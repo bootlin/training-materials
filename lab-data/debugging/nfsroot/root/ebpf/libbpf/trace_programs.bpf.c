bootlin/training-materials

commit 970e97b4a8cdd3a3b050275a9c35c44b2f121f0e

labs: debugging: use the exact execve prototype for ebpf lab
Trainees may ask why the ebpf program we are defining do not have the
exact same prototype as the corresponding system call.

Prevent this question by using the exact same prototype as the one seen
in `man execve`

Signed-off-by: Alexis Lothoré <alexis.lothore@bootlin.com>

lab-data/debugging/nfsroot/root/ebpf/libbpf/trace_programs.bpf.c
@@ -6,7 +6,9 @@
 #define MAX_FILENAME_LEN 32
 
 SEC("kprobe/sys_execve")
-int BPF_KPROBE(trace_execve, char *pathname, char *argv[], char *envp[])
+int BPF_KPROBE(trace_execve, const char *path, char *const _Nullable argv[],
+               char *const _Nullable envp[])
+
 {
 	int pid = bpf_get_current_pid_tgid() & 0xFFFFFFFF;
 	char fmt[] = "New process %d running program %s";

commit fdfcad5b0e8268a938a845dade40c10e7ebe425d

lab-data/debugging: fix wrong variable name
The prototype used in the small libbpf program has been updated to
reflect the exact prototype of the targeted function, but the
corresponding variable usage has not been updated accordingly, leading
to a build failure. Update the relevant usage site to fix it.

Fixes: 970e97b4a8cd ("labs: debugging: use the exact execve prototype for ebpf lab")
Signed-off-by: Alexis Lothoré <alexis.lothore@bootlin.com>

lab-data/debugging/nfsroot/root/ebpf/libbpf/trace_programs.bpf.c
@@ -13,7 +13,7 @@ int BPF_KPROBE(trace_execve, const char *path, char *const _Nullable argv[],
 	int pid = bpf_get_current_pid_tgid() & 0xFFFFFFFF;
 	char fmt[] = "New process %d running program %s";
 
-	bpf_trace_printk(fmt, sizeof(fmt), pid, pathname);
+	bpf_trace_printk(fmt, sizeof(fmt), pid, path);
 	return 0;
 }
 