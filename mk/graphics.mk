708e2460febd543800f5e8be22f2537178d04ecd
Paul Kocialkowski <paulk@sys-base.io>
2025-11-24 21:03:37 +0100

    slides: graphics: Split files into subsections
    Signed-off-by: Paul Kocialkowski <paulk@sys-base.io>

--- mk/graphics.mk
+++ mk/graphics.mk
@@ -2,8 +2,23 @@ GRAPHICS_SLIDES = \
 	first-slides \
 	about-us \
 	course-information \
-	graphics-theory \
-	graphics-hardware \
-	graphics-software \
+	graphics-theory-image \
+	graphics-theory-color \
+	graphics-theory-layout-formats \
+	graphics-theory-pixel-drawing \
+	graphics-theory-pixel-operations \
+	graphics-hardware-overview \
+	graphics-hardware-pipelines \
+	graphics-hardware-display-devices \
+	graphics-hardware-display-interfaces \
+	graphics-hardware-render \
+	graphics-hardware-system \
+	graphics-software-stack-overview \
+	graphics-software-linux-tty \
+	graphics-software-linux-fbdev \
+	graphics-software-linux-drm \
+	graphics-software-userspace-x \
+	graphics-software-userspace-wayland \
+	graphics-software-userspace-mesa \
 	last-slides \
 	graphics-extra
