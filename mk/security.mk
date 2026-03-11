1ed2805d6d53e1aa1c5afbcaa36ce54d1c1d92ae
Thomas Petazzoni <thomas.petazzoni@bootlin.com>
2026-03-11 15:52:21 +0100

    Add initial details about security course
    For now, only the agenda, and an empty skeleton for the slides and
    labs. This is just to ensure we can build everything.
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

---
+++ mk/security.mk
@@ -0,0 +1,10 @@
+SECURITY_SLIDES = \
+	first-slides \
+	about-us \
+	course-information-title \
+	course-information \
+	shopping-list-imx93-frdm \
+	last-slides
+
+SECURITY_IMX93_FRDM_LABS = \
+	setup
