783d137575dd6452db276c6f08cb3e618601b032
Olivier Benjamin <olivier.benjamin@bootlin.com>
2026-05-06 16:40:46 +0200

    Security training: add skeleton
    Signed-off-by: Olivier Benjamin <olivier.benjamin@bootlin.com>

--- mk/security.mk
+++ mk/security.mk
@@ -4,7 +4,15 @@ SECURITY_SLIDES = \
 	course-information-title \
 	course-information \
 	shopping-list-imx93-frdm \
+	security-fundamental-concepts \
+	security-hw-sec-barriers \
+	security-secure-boot-part-1 \
+	security-hw-sec-barriers \
+	security-secure-boot-part-1 \
+	security-secure-boot-part-2 \
+	security-confidential-information \
+	security-userland \
+	security-measured-boot \
+	security-compliance \
+	security-ab-updates \
 	last-slides
-
-SECURITY_IMX93_FRDM_LABS = \
-	setup
